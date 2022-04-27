3a55020a6981530073d3d8d7d538b0b2da7811a5
zhou-sheng <8871908+little-austin@user.noreply.gitee.com>
2022-04-27 08:50:55 +0800

    first commit 🥩

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # nodejs-local-mock
-Jsonp local simulation data
+>   Jsonp local simulation data
+
+## Mock 数据
+
+1. 开启服务器 nodemon node-server.js + enter.
+2. `http://localhost:8080/mock/${filename}` 请求地址
+3. 请求方式 jsonp 回调名: callback 
+4. `node-server` 文件中 `DIRECTORY` 常量表示当前调试的目录, 比如现在等于 INTEGRAL_MOCK 表示调试的 integral-mock 目录
+
+
+## Usage
+请求方式 `jsonp`, query 数据中携带回调函数名为 `callback`;
+```javascript
+let loading = null
+$.ajax({
+    url: 'http://localhost:8080/mock/' + 目录名 + 文件名,
+    data: {},
+    dataType: 'jsonp',
+    jsonpCallback: 'jsonp_callback' + 随机串,
+    method: 'post',
+    beforeSend() {
+        loading = layer.load(1, {
+            shade: [0.3, '#000']
+        });
+    }
+}).done(function (res) {
+    layer.close(loading)
+    if (res.code != 0) return;
+    
+})
+```
---
+++ node-server.js
@@ -0,0 +1,43 @@
+'use strict';
+let http = require('http'); // 通过 require 将 http 库包含到程序中
+let url = require('url'); // 引入 url 模块解析 url 字符串
+let querystring = require('querystring'); // 引入 querystring 模块处理 query 字符串
+let server = http.createServer(); // 创建新的 HTTP 服务器
+
+const INTEGRAL_MOCK = 'integral-mock';
+const ASSION_GROUPS = 'assion-groups';
+const DRILLMASTER_TRAINING  = 'drillmaster-training'
+const DATA_PANEL = 'data-panel';
+
+const DIRECTORY = DATA_PANEL; // 当前测试目录模块;
+
+// 监听服务器请求事件
+server.on('request', function (req, res) {
+    let urlPath = url.parse(req.url).pathname;
+    let qs = querystring.parse(req.url.split('?')[1]);
+
+    // if (urlPath === '/mock' && qs.callback) {
+
+    if (qs.callback) {
+        res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
+
+        let filename = urlPath.replace(/\/mock\//, '');
+        let data = require(`./${DIRECTORY}/${filename}`);
+
+        data = JSON.stringify(data);
+        let callback = qs.callback + '(' + data + ');';
+
+        console.log(callback, 'Response !');
+        res.end(callback);
+    }
+
+    else {
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+        res.end('Not the jsonp\n');
+    }
+
+});
+
+server.listen('8080', () => {
+    console.log('Server running! 8080');
+});
